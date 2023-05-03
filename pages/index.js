537229d714130628cb1acb00d795a2624ea0be9f
Sergio Pereira Sena <senanetworker@gmail.com>
2023-05-03 18:12:03 -0300

    lab

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,52 @@
 import Pessoa from "@/components/Pessoa";
 import { useState } from "react";
+import comAutorizacao from "@/hoc/comAutorizacao";
 
-
-export default function Home() {
+function Home() {
   const [idade, setIdadeSergio] = useState(17);
+  //Conceito de looping
+  const pessoas = [
+    {
+      nomePessoa: 'Davi',
+      idadePessoa: 28
+    },
+    {
+      nomePessoa: 'Camila',
+      idadePessoa: 17
+    },
+    {
+      nomePessoa: 'wagner',
+      idadePessoa: 23
+    }
+  ];
 
   const aumentaIdadeSergio = () => {
-   console.log('aumentaIdadeSergio')
-    setIdadeSergio(1+idade)
+    console.log('aumentaIdadeSergio')
+    setIdadeSergio(1 + idade)
   }
-
   return (
+    //Variavel com a env publica
     <>
-      <h1>Ola mundo!</h1>
+      <div>
+        <h2>Ola mundo! {process.env.NEXT_PUBLIC_TESTE}</h2>
+
+
+      </div>
       <Pessoa nome='Sergio' idade={idade} />
       <Pessoa nome='kennedy' idade={35} />
+
+
+
+      {pessoas.map(({ nomePessoa, idadePessoa }, index) => {
+        return <Pessoa
+          nome={nomePessoa}
+          idade={idadePessoa}
+          key={index}
+        />
+      })}
       <button onClick={aumentaIdadeSergio}>Aumenta Idade sergio</button>
     </>
   );
 }
+
+export default comAutorizacao(Home)
